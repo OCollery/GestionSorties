dd4249c14ea552c2879732cb1f94e65d6d89d112
Madame-Mim <emilie.davi2020@campus-eni.fr>
2021-03-01 16:36:37 +0100

    emilie campus et ville entity

---
+++ src/Entity/Ville.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VilleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VilleRepository::class)
+ */
+class Ville
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     * @ORM\ManyToOne(targetEntity=Article::Lieu, inversedBy="nom")
+     */
+    private $nomVille;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $codePostal;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomVille(): ?string
+    {
+        return $this->nomVille;
+    }
+
+    public function setNomVille(string $nomVille): self
+    {
+        $this->nomVille = $nomVille;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->codePostal;
+    }
+
+    public function setCodePostal(string $codePostal): self
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+}
