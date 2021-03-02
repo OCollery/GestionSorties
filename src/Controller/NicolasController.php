ad7c194f713e82895d4d89cfee5768e012680fa1
nicolas <nicolai_ii@msn.com>
2021-03-02 18:14:18 +0100

    maj création sortie

--- src/Controller/NicolasController.php
+++ src/Controller/NicolasController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Etat;
+use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Entity\Ville;
 use App\Form\CreationSortieType;
 use App\Repository\EtatRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -11,18 +13,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class NicolasController extends AbstractController
 {
     /**
      * @Route("/creerSortie", name="creer_sortie")
      */
-    public function creationSortie(Request $request, EntityManagerInterface $em)
+    public function creationSortie(Request $request, EntityManagerInterface $em /* ,UserInterface $user pour récup l'utilsateur */)
     {
        // $this->denyAccessUnlessGranted("ROLE_USER");
+       // $userId = $user->getId(); récupération de l'id utilisateur
 
         $sortie = new Sortie();
-
+     //   $sortie-> setOrganisateur($userId); rendre l'utilisateur organisateur
         $createSortieForm = $this->createForm(CreationSortieType::class, $sortie);
 
         $createSortieForm->handleRequest($request);
