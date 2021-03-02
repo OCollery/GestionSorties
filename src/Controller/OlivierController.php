2de9cac93b168f089a62e0ec1fcf3187b10a03e4
OCollery <olivier.collery2020@campus-eni.fr>
2021-03-02 11:31:04 +0100

    début formulaire update

--- src/Controller/OlivierController.php
+++ src/Controller/OlivierController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\UpdateType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,15 +13,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class OlivierController extends AbstractController
 {
     /**
-     * @Route("/Modifier_Sortie", name="modifier")
+     * @Route("/Modifier_Sortie{id}", name="modifier")
      */
-    public function modifier(): Response
+    public function modifier(EntityManagerInterface $em, Request $request, Sortie $sortie): Response
     {
-        return $this->render('main/modifierUneSortie.html.twig');
+    //Permet de récupérer l'ensemble des sorties
+        $repo = $em->getRepository(Sortie::class);
+        $sortie = $repo ->findAll();
+
+    //on récupère le formulaire
+      /*  $form = $this->createForm(UpdateType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+           $em->flush();
+
+           $this->addFlash('success', 'La sortie a été mise à jour');
+           return $this->redirectToRoute('home');
+        }*/
+        $formView = $form->createView();
+
+        return $this->render('main/modifierUneSortie.html.twig',[array('sortie'=>$sortie, 'updateForm'=>$formView)]);
     }
 
-    public function update(EntityManagerInterface $em, Request $request)
-    {
-
-    }
 }
