692dfad2ab8cd8a0087112f4cbd57cf4c03a4b92
nicolas <nicolai_ii@msn.com>
2021-03-05 09:26:44 +0100

    commit oublié e la veille

---
+++ src/Form/RechercheSortieType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RechercheSortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('campus')
+            ->add('nom')
+            ->add('dateHeureDebut')
+            ->add('dateHeureDebut')
+            ->add('organisateur')
+            /*->add('inscrit')
+            ->add('pas inscrit')
+            ->add('passées')*/
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
