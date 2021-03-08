4dffb14c3cadc65ec88d807b27b314aa2cd8c61f
Madame-Mim <emilie.davi2020@campus-eni.fr>
2021-03-08 11:40:33 +0100

    rectification petite connerie...

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\VilleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin", name="admin_")
+ */
+class AdminController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route ("/villes", name="villes")
+     */
+    public function gererVilles(VilleRepository $villes)
+    {
+        return $this->render("admin/villes.html.twig", [
+
+    ]);
+    }
+
+}
